708e41cd3926ea93a0f632dbccb801e213002e9f
zxr <my@163.com>
2019-07-31 22:58:03 +0800

    xxx

---
+++ common/build_uboot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pushd ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/opensource/uboot/u-boot-2010.06/
+make ARCH=arm CROSS_COMPILE=${CROSS_COMPILER_PREFIX}- ${BOARD_UBOOT_CONFIG}
+make ARCH=arm CROSS_COMPILE=${CROSS_COMPILER_PREFIX}- -j
+cp u-boot.bin ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/tools/pc/uboot_tools/
+pushd ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/tools/pc/uboot_tools/
+chmod +x mkboot.sh
+./mkboot.sh reg_info_hi3531d.bin u-boot-hi3531d.bin
+if [ ! -d ${TOP_DIRECTOR}/build/out/ ];then
+    mkdir ${TOP_DIRECTOR}/build/out/
+fi
+cp u-boot-hi3531d.bin  ${TOP_DIRECTOR}/build/out/
+popd
+
+
+
+popd
