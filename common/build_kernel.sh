cfc3eaae929c0d03cbcb02de44ef62f33ef3f6d4
zxr <my@163.com>
2019-08-01 14:18:58 +0800

    xxx

---
+++ common/build_kernel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pushd ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/opensource/kernel/linux-3.18.y/
+if [ -d ${TOP_DIRECTOR}/resource/kernel/${KERNEL_CONFIG} ];then
+    cp ${TOP_DIRECTOR}/resource/kernel/${KERNEL_CONFIG} arch/arm/configs/
+fi
+make ARCH=arm CROSS_COMPILE=${CROSS_COMPILER_PREFIX}- ${KERNEL_CONFIG}
+make ARCH=arm CROSS_COMPILE=${CROSS_COMPILER_PREFIX}- -j uImage
+cp arch/arm/boot/uImage ${TOP_DIRECTOR}/build/out/
+
+
+
+popd
