1f6a667bc14db3f9fff3e09098ceceb4944d341c
zxr <zxr13027502030@163.com>
2019-08-03 16:27:27 +0800

    xxx

--- common/build_busybox.sh
+++ common/build_busybox.sh
@@ -3,26 +3,29 @@
 pushd ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/opensource/busybox/busybox-1.20.2/
 
 
-if [ -f ${TOP_DIRECTOR}/resource/busybox/${BUSYBOX_CONFIG} ];then
-    cp ${TOP_DIRECTOR}/resource/busybox/${BUSYBOX_CONFIG} ./
-fi
-cp ${BUSYBOX_CONFIG} .config
+#if [ -f ${TOP_DIRECTOR}/resource/busybox/${BUSYBOX_CONFIG} ];then
+#    cp ${TOP_DIRECTOR}/resource/busybox/${BUSYBOX_CONFIG} ./
+#fi
+#cp ${BUSYBOX_CONFIG} .config
 
-make -j
-rm _install -rf
-make install
+#make -j
+#rm _install -rf
+#make install
 
-pushd _install
-ln -s sbin/init ./
+#pushd _install
 #mkdir etc dev lib tmp var mnt home proc
-SYSROOT=$(${CROSS_COMPILER_PREFIX}-gcc --print-sysroot)
+#SYSROOT=$(${CROSS_COMPILER_PREFIX}-gcc --print-sysroot)
 #cp ${SYSROOT}/lib/* ./lib/ -rfd
-popd
+#popd
 
 if [  -d ${TOP_DIRECTOR}/build/out/rootfs ];then
     rm ${TOP_DIRECTOR}/build/out/rootfs -rf
 fi
 mkdir ${TOP_DIRECTOR}/build/out/rootfs 
+
+if [ ! -d ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/pub/rootfs_uclibc/ ];then
+    tar xvf ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/pub/rootfs_uclibc.tgz -C ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/pub/
+fi
 
 cp ${TOP_DIRECTOR}/build/${HI3531D_SDK}/osdrv/pub/rootfs_uclibc/* ${TOP_DIRECTOR}/build/out/rootfs/ -rfd
 mkdir ${TOP_DIRECTOR}/build/out/rootfs/var/empty
