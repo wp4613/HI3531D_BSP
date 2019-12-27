3e65c95c7831bd934586cc4d53360dacbf9b245c
Your Name <you@example.com>
2019-12-27 17:39:45 +0800

    xxx

--- common/build_mvr_upgrade.sh
+++ common/build_mvr_upgrade.sh
@@ -15,9 +15,6 @@ rm ${ROOTFS}/usr/H9-MVR/configs/config.ini
 #mkdir ${ROOTFS}/usr/H9-MVR/configs/ -p
 #mkdir ${ROOTFS}/bin/ -p
 #cp ${TOP_DIRECTOR}/resource/hidoo/usr/H9-MVR/configs/version.txt ${ROOTFS}/usr/H9-MVR/configs/
-echo "#!/bin/sh" > ${ROOTFS}/bin/hellow.sh
-echo "echo hellow" > ${ROOTFS}/bin/hellow.sh
-chmod +x ${ROOTFS}/bin/hellow.sh
 
 chmod +x ${TOP_DIRECTOR}/resource/upgrade.sh
 cp ${TOP_DIRECTOR}/resource/upgrade.sh ${ROOTFS}/ -rfd
